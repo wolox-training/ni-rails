779b4b66c45402243f550abff0a551f26049e85c
NiGiInRoAcc <nicolas.g.infante@accenture.com>
2022-06-08 10:12:31 -0500

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,13 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Wbooks
+  # comment
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -16,7 +17,7 @@ module Wbooks
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = 'Central Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
